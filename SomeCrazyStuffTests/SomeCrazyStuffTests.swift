4f21383be1a35f4e0f2837ac755d41d8cc77415d
王笛 Wang,Di <wd1998313@gmail.com>
2016-11-19 12:52:11 -0500

    Initial Commit

---
+++ SomeCrazyStuffTests/SomeCrazyStuffTests.swift
@@ -0,0 +1,36 @@
+//
+//  SomeCrazyStuffTests.swift
+//  SomeCrazyStuffTests
+//
+//  Created by Di Wang on 2016/11/19.
+//  Copyright © 2016年 Di Wang. All rights reserved.
+//
+
+import XCTest
+@testable import SomeCrazyStuff
+
+class SomeCrazyStuffTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
